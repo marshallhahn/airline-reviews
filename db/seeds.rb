430d9de98cd509bf8a26cc00502f87bfcbbdde9e
Marshall Hahn <m.hahn@calendly.com>
2021-04-22 18:19:13 -0500

    Seed the database with a few airlines and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: "Great airline!",
+    description: "I had a lovely flight. Simply perfect in every way.",
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: "Bad airline",
+    description: "I had a bad flight. The restroom was smelly and babies were crying the entire time.",
+    score: 1,
+    airline: airlines.first
+  }
+])
